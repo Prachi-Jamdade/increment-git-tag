5b90832bcda57c9ae93904b56ae09a07952ef68b
Prachi <prachi.jamdade2390@gmail.com>
2024-05-12 18:51:24 +0530

    correct separator

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function run() {
     const versionType = core.getInput("version-type");
     const src = __dirname;
 
-    await exec.exec(`${src}\git_update.sh -v ${versionType}`);
+    await exec.exec(`${src}\\git_update.sh -v ${versionType}`);
   } catch (error) {
     core.setFailed(error.message);
   }
